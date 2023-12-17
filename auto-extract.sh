520719196f6ea9abcc0ad0f5d769b8a20985adb8
JaKooLit <jimmielovejay.com>
2023-12-17 18:12:09 +0900

    updated script and readme

--- auto-extract.sh
+++ auto-extract.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.icons
 mkdir -p ~/.themes
-unzip -qo "GTK-themes-icons/theme/Tokyonight-Dark-BL-LB.zip" -d ~/.themes
-unzip -qo "GTK-themes-icons/theme/Tokyonight-Light-B-LB.zip" -d ~/.themes
-unzip -qo "GTK-themes-icons/icon/Tokyonight-Dark-Icons.zip" -d ~/.icons
-unzip -qo "GTK-themes-icons/icon/Tokyonight-Light-Icons.zip" -d ~/.icons
+unzip -qo "theme/Tokyonight-Dark-BL-LB.zip" -d ~/.themes
+unzip -qo "theme/Tokyonight-Light-B-LB.zip" -d ~/.themes
+unzip -qo "icon/Tokyonight-Dark-Icons.zip" -d ~/.icons
+unzip -qo "icon/Tokyonight-Light-Icons.zip" -d ~/.icons
